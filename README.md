426ed76475b68284ad281195b70cd659b89c2932
Douglas Dias <douglas.dias2102@outlook.com>
2019-07-19 07:20:25 -0300

    feat:#1 Case where K is followed by a single character

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # string_reduction_machine_with_2_arrays
 An implementation of a String Reduction Machine with Two Arrays
+
+Branches:
+    feat/0: Initial C source file
+    feat/1: K operator implementation
--- srm_2a.c
+++ srm_2a.c
@@ -7,7 +7,60 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-main(){
-	printf("First Commit");
+char INPUT[100000000] = /*"S(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))";*/ "KS(KU)V";
+char OUTPUT[100000000] = "";
+
+void K(char in[], char out[]){
+	if(in[1] != '('){
+		int i = 2;
+		out[0] = in[1];
+		if(in[2] != '('){
+			out[1] = in[3];
+			while(in[i + 2] != '\0'){
+				out[i] = in[i + 2];
+				i++;
+			}
+			out[i] = '\0';
+		} else {
+			int outPointer = 1;
+			int openParenthesis = 1;
+			i++;
+			while(openParenthesis != 0){
+				switch(in[i]){
+					case '(':
+						openParenthesis++;
+						break;
+					case ')':
+						openParenthesis--;
+					default:
+						break;
+				}
+				i++;
+			}
+			while(in[i] != '\0'){
+				out[outPointer] = in[i];
+				outPointer++;
+				i++;
+			}
+			out[outPointer] = '\0';
+		}
+	} else {
+		
+	}
+}
+
+void NextOperation(char in[], char out[]){
+	switch(in[0]){
+		case 'K':
+			K(in, out);
+			break;	
+		default:
+			return;
+	}
+}
+
+int main(){
+	NextOperation(INPUT, OUTPUT);
+	printf("%s", &OUTPUT);
 	return 0;
 }
