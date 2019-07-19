f76ce31565462852dc620612cf93c2a62300ea1e
Douglas Dias <douglas.dias2102@outlook.com>
2019-07-18 21:59:17 -0300

    feat:#0 Estrutura Inicial

---
+++ srm_2a.c
@@ -0,0 +1,13 @@
+/*
+*	Author: Douglas Dias da Silva
+*	Date:	18/07/2019
+*	Email: douglas.dias2102@outlook.com
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+main(){
+	printf("First Commit");
+	return 0;
+}
