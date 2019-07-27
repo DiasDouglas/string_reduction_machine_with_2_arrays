47e0fe7d7fec859c262a32147518a45e07676d23
Douglas Dias <douglas.dias2102@outlook.com>
2019-07-27 11:55:46 -0300

    feat: #2 Cases 'SABC' and 'SAB(C[C)'

--- srm_2a.c
+++ srm_2a.c
@@ -7,7 +7,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-char INPUT[100000000] = /*"S(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))";*/ "K(KU)(VK)LYTY";
+char INPUT[100000000] = /*"S(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))";*/ "SAB(CHJH)JHJ";
 char OUTPUT[100000000] = "";
 
 void K(char in[], char out[]){
@@ -90,11 +90,85 @@ void K(char in[], char out[]){
 	}
 }
 
+void S(char in[], char out[]){
+	if(in[1] != '('){
+		if(in[2] != '('){
+			if(in[3] != '('){
+				out[0] = '(';
+				out[1] = in[1];
+				out[2] = in[3];
+				out[3] = ')';
+				out[4] = '(';
+				out[5] = in[2];
+				out[6] = in[3];
+				out[7] = ')';
+				
+				int i, j = 8;
+				for(i = 4; in[i] != '\0'; i++){
+					out[j] = in[i];	
+					j++;
+				}
+				out[j] = '\0';
+			} else {
+				int cStart = 3;
+				int cEnd = 4;
+				int openParenthesis = 1;
+				while(openParenthesis != '\0'){
+					if(in[cEnd] == '(')
+						openParenthesis++;
+					else if(in[cEnd] == ')')
+						openParenthesis--;
+					cEnd++;
+				}
+				
+				out[0] = '(';
+				out[1] = in[1];
+				
+				int iteratorIn;
+				int iteratorOut = 2;
+				for(iteratorIn = cStart; iteratorIn < cEnd; iteratorIn++){
+					out[iteratorOut] = in[iteratorIn];
+					iteratorOut++;
+				}
+				
+				out[iteratorOut] = ')';
+				iteratorOut++;
+				out[iteratorOut] = '(';
+				iteratorOut++;
+				out[iteratorOut] = in[2];
+				iteratorOut++;
+				
+				for(iteratorIn = cStart; iteratorIn < cEnd; iteratorIn++){
+					out[iteratorOut] = in[iteratorIn];
+					iteratorOut++;
+				}
+				
+				out[iteratorOut] = ')';
+				iteratorOut++;
+				
+				for(iteratorIn = cEnd; in[iteratorIn] != '\0'; iteratorIn++){
+					out[iteratorOut] = in[iteratorIn];	
+					iteratorOut++;
+				}
+				
+				out[iteratorOut] = '\0';
+				
+			}
+		} else {
+			
+		}
+	} else {
+		
+	}
+}
 void NextOperation(char in[], char out[]){
 	switch(in[0]){
 		case 'K':
 			K(in, out);
 			break;	
+		case 'S':
+			S(in, out);
+			break;
 		default:
 			return;
 	}
